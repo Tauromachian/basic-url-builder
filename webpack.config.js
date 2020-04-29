44c9051bb6e22d9ed6eb04d292ec94bf86f8d742
jogarcia <jogarcia@uclv.cu>
2020-04-29 15:17:12 -0400

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+var commonJsConfig = {
+  target: 'node',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'basic-url-builder.node.js',
+    libraryTarget: 'commonjs'
+  }
+};
+
+var browserConfig = {
+  target: 'web',
+  mode: 'production',
+  output: {
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'basic-url-builder.min.js'
+  }
+};
+
+module.exports = [commonJsConfig, browserConfig];
