c35daa69aa9aa4a109077bb42b97a24fcb07eaa8
jogarcia <you@example.com>
2020-05-17 22:39:59 -0400

    Create RouteBuilder class

---
+++ src/RouteBuilder.js
@@ -0,0 +1,44 @@
+class RouteBuilder {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+    this.routes = new Map();
+  }
+
+  /**
+   * Creates the complete named route that can be later on used
+   * @param {String} url Url of the route
+   * @param {String} routeName Name Of the route
+   */
+  static setRoute(url, routeName) {
+    if (routeName) {
+      this.routes.set(routeName, this._makeUrl(url));
+      return 1;
+    } else {
+      this.routes.set(url, this._makeUrl(url));
+      return 1;
+    }
+  }
+
+  /**
+   * Gets the route using the name
+   * @param {String} routeName Name of the route or the route itself if no name was provided at insert
+   */
+  static getRoute(routeName) {
+    return this.routes.get(routeName);
+  }
+
+  static _makeUrl(url) {
+    const newUrl = this.defaultBaseUrl + url;
+    return newUrl;
+  }
+
+  /**
+   * Checks if the route was created
+   * @param {String} routeName Name of the route
+   */
+  static isRouteCreated(routeName) {
+    return this.routes.has(routeName);
+  }
+}
+
+module.exports = RouteBuilder;
