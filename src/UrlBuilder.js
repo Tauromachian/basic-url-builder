800f1e181eba942f0c4b2788b59c6cffdd8b4f96
jogarcia <you@example.com>
2020-05-17 22:36:05 -0400

    Use RouteBuilder class

--- src/UrlBuilder.js
+++ src/UrlBuilder.js
@@ -1,23 +1,24 @@
-class UrlBuilder {
+const RouteBuilder = require("./RouteBuilder");
 
+class UrlBuilder {
   /**
    * Sets the base url of the UrlBuilder
    * @param {String} url Base url
    */
-  static setBaseUrl(url, name="def") {
-    if(typeof url !== "string"){
+  static setBaseUrl(url, name = "def") {
+    if (typeof url !== "string") {
       return 1;
     }
 
-    this.defaultBaseUrl = url;
+    this.defaultBaseUrl = new RouteBuilder(url);
     this.baseUrls = new Map();
-    this.baseUrls.set(this.defaultBaseUrl, name)
+    this.baseUrls.set(this.defaultBaseUrl, name);
     this.routes = new Map();
     return 0;
   }
 
   static getBaseUrl(name) {
-    if(!name){
+    if (!name) {
       return this.defaultBaseUrl;
     }
     return this.baseUrls.get(name);
@@ -26,20 +27,14 @@ class UrlBuilder {
   static addBaseUrl(url, name) {
     this.baseUrls.set(url, name);
   }
-  
+
   /**
    * Creates the complete named route that can be later on used
    * @param {String} url Url of the route
    * @param {String} routeName Name Of the route
    */
   static setRoute(url, routeName) {
-    if (routeName) {
-      this.routes.set(routeName, this._makeUrl(url));
-      return 1;
-    } else {
-      this.routes.set(url, this._makeUrl(url));
-      return 1;
-    }
+    this.defaultBaseUrl.setRoute(url, routeName);
   }
 
   /**
@@ -47,7 +42,7 @@ class UrlBuilder {
    * @param {String} routeName Name of the route or the route itself if no name was provided at insert
    */
   static getRoute(routeName) {
-    return this.routes.get(routeName);
+    this.defaultBaseUrl.getRoute(routeName);
   }
 
   /**
@@ -56,11 +51,6 @@ class UrlBuilder {
    */
   static isRouteCreated(routeName) {
     return this.routes.has(routeName);
-  }
-
-  static _makeUrl(url) {
-    const newUrl = this.defaultBaseUrl + url;
-    return newUrl;
   }
 }
 
