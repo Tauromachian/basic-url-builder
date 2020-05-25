fc87b3c306573f9190c18cdf0e2675aa3023a43b
jogarcia <jogarcia@uclv.cu>
2020-05-25 16:14:05 -0400

    Create basic RouteBuilderHandler

---
+++ src/RouteBuilderHandler.js
@@ -0,0 +1,44 @@
+const AbstractRouteBuilder = require("./AbstractRouteBuilder");
+
+class RouteBuilderHandler extends AbstractRouteBuilder {
+  constructor(routeSet, name) {
+    this.defaultUrlSet = routeSet;
+    this.routes = {
+      [name]: routeSet
+    };
+  }
+
+  setBaseUrl(url, name) {
+    if(this.routes[name]){
+      this.defaultUrlSet = this.routes[name];
+      this.routes[name].setBaseUrl(url);
+    }
+    this.routes[name] = calculateRoutes();
+    this.routes[name].setBaseUrl(url);
+  }
+
+  getBaseUrl(name) {
+    return this.routes[name].name;
+  }
+
+  addRoute(url, name) {
+    if (name) {
+      this.routes.set(name, this._makeUrl(url));
+      return 1;
+    } else {
+      this.routes.set(url, this._makeUrl(url));
+      return 1;
+    }
+  }
+
+  getRoute(name) {
+    return this.defaultUrlSet.getRoute(name);
+  }
+
+  isRouteCreated(name) {
+    return this.defaultUrlSet.isRouteCreated(name);
+  }
+
+}
+
+module.exports = RouteBuilderHandler;
