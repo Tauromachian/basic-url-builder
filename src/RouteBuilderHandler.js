099dddfaea5f8f81df8b25f206b052fc85e3dda8
jogarcia <you@example.com>
2020-06-01 16:38:20 -0400

    Take out unnecesary methods

--- src/RouteBuilderHandler.js
+++ src/RouteBuilderHandler.js
@@ -43,32 +43,6 @@ class RouteBuilderHandler extends AbstractRouteBuilder {
   isRouteCreated(name) {
     return this.defaultUrlSet.isRouteCreated(name);
   }
-
-  _makeUrl(url, baseUrl) {
-    const newUrl = baseUrl + url;
-    return newUrl;
-  }
-
-  _calculateRoutes() {
-    let routes = {};
-    for (const key in this.routes) {
-      if (Object.prototype.hasOwnProperty.call(this.routes, key)) {
-        const routeBuilder = this.routes[key];
-        const baseUrl = routeBuilder.getBaseUrl();
-        const routeMap = routeBuilder.routes;
-        const keys = routeMap.keys();
-        for (const key of keys) {
-           const route = this._getRouteWithoutBaseUrl(routeMap.get(key), baseUrl);
-           routes[key] = this._makeUrl(route, baseUrl);
-        }
-      }
-    }
-  }
-
-  _getRouteWithoutBaseUrl(url, baseUrl) {
-    return url.replace(baseUrl, "");
-  }
-
 }
 
 module.exports = RouteBuilderHandler;
