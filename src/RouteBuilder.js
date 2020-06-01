99608e05cd69cd37a7fce92608ef1372649ab15d
jogarcia <you@example.com>
2020-06-01 14:08:12 -0400

    Calculate routes when setBaseUrl is used

--- src/RouteBuilder.js
+++ src/RouteBuilder.js
@@ -8,7 +8,12 @@ class RouteBuilder extends AbstractRouteBuilder {
   }
 
   setBaseUrl(url) {
+    if (!url) {
+      return 0;
+    }
+    const oldBaseUrl = this.baseUrl;
     this.baseUrl = url;
+    this._calculateRoutes(oldBaseUrl);
   }
 
   getBaseUrl() {
@@ -16,22 +21,23 @@ class RouteBuilder extends AbstractRouteBuilder {
   }
 
   addRoute(route, name) {
-    if(!route){
+    if (!route) {
       return 0;
     }
-    if(!name){
+    if (!name) {
       return 0;
     }
     this.routes.set(name, this._makeUrl(route));
     return 1;
   }
 
-  addRoutes(routes){
+  addRoutes(routes) {
     for (const key in routes) {
       if (Object.prototype.hasOwnProperty.call(routes, key)) {
-        this.routes.set(key, routes[key]);        
+        this.routes.set(key, routes[key]);
       }
     }
+    return 1;
   }
 
   getRoute(routeName) {
@@ -40,6 +46,23 @@ class RouteBuilder extends AbstractRouteBuilder {
 
   isRouteCreated(routeName) {
     return this.routes.has(routeName);
+  }
+
+  _calculateRoutes(oldBaseUrl) {
+    let routes = new Map();
+    const keys = this.routes.keys();
+
+    for (const key of keys) {
+      const completeUrl = this.routes.get(key);
+      const route = this._getRouteWithoutBaseUrl(completeUrl, oldBaseUrl);
+      const newCompleteUrl = this._makeUrl(route);
+      routes.set(key, newCompleteUrl);
+    }
+    this.routes = routes;
+  }
+
+  _getRouteWithoutBaseUrl(url, baseUrl) {
+    return url.replace(baseUrl, "");
   }
 
   _makeUrl(url) {
