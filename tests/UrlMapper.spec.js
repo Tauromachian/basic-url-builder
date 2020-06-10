1b1b23965d065d74a7110aaf44023c91adc8b052
jogarcia <you@example.com>
2020-06-09 21:40:29 -0400

    Check for correct inserts

--- tests/UrlMapper.spec.js
+++ tests/UrlMapper.spec.js
@@ -28,4 +28,20 @@ describe("Url Mapper.js", function () {
     expect(routes.get("dev")).to.exist;
     expect(routes.get("prod")).to.exist;
   });
+
+  it("Should return the right urls", function () {
+    expect(routes.get("dev").getRoute("service")).to.equal(
+      "http://firsttesturl/service"
+    );
+    expect(routes.get("dev").getRoute("products")).to.equal(
+      "http://firsttesturl/products"
+    );
+    routes.get("prod").setDefaultSet("prod");
+    expect(routes.get("prod").getRoute("service")).to.equal(
+      "http://secondtesturl/service"
+    );
+    expect(routes.get("prod").getRoute("products")).to.equal(
+      "http://secondtesturl/products"
+    );
+  });
 });
